28e91f91fe2382076d365ab75c28ad0507079beb
amiramar1368 <a.amarloo68@gmail.com>
2025-03-01 13:36:06 +0330

    finish all category controller

--- controller/category-controller.js
+++ controller/category-controller.js
@@ -1,7 +1,10 @@
 import { Op } from "@sequelize/core";
 import Category from "../model/category-model.js";
+import Good from "../model/good-model.js";
+import sequelize from "../utils/db.js";
 
 export class CategoryController {
+  
   static async addCategory(req, res) {
     const { name } = req.body;
     if (name != undefined) {
@@ -32,9 +35,54 @@ export class CategoryController {
     }
   }
 
-  static async getCategoryById(req,res){}
+  static async getCategoryById(req, res) {
+    const id = +req.params.id;
+    try {
+      const category = await Category.findByPk(id);
+      if (category) {
+        return res.sendSuccessResponse(201, category, "category fetched successfully");
+      } else {
+        return res.sendFailureResponse(404, "category not found");
+      }
+    } catch (err) {
+      res.sendError(err);
+    }
+  }
 
-  static async updateCategory(req,res){}
+  static async updateCategory(req, res) {
+    const id = +req.params.id;
+    const name = req.body.name;
+    try {
+      const category = await Category.findByPk(id);
+      if (!category) return res.sendFailureResponse(404, "category not found");
+      category.update({
+        name
+      })
+      return res.sendSuccessResponse(201, null, "category updated successfully");
+    } catch (err) {
+      res.sendError(err)
+    }
+  }
 
-  static async deleteCategory(req,res){}
+  static async deleteCategory(req, res) {
+    const id = + req.params.id;
+    try {
+      const category = await Category.findByPk(id);
+      if(!category) return res.sendFailureResponse(404,"category not found")
+    } catch (err) {
+      
+    }
+    const force = req.query.force;
+    const good = await Good.findOne({ where: { categoryId: id } });
+    if (!good || (good && force === "true")) {
+      await sequelize.transaction(async () => {
+        await Good.destroy({ where: { categoryId: id } });
+        await Category.destroy({ where: { id } });
+        return res.sendSuccessResponse(202, null, "category and its good delete successfully");
+      })
+    } else {
+      res.sendFailureResponse(400, "this category has some good and you cant remove it")
+    }
+
+  }
 }
